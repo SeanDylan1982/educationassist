3930923bb518779c16d682669b50938cfbbba14f
SeanDylan1982 <74496368+SeanDylan1982@users.noreply.github.com>
2023-02-08 09:38:42 +0200

    react components init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import ImageMasonry from './masonry';
+import ButtonAppBar from './appBar';
+import FixedBottomNavigation from './bottomNav';
+import ActiveLastBreadcrumb from './breadcrumbs';
+import PaginationOutlined from './pagination';
 
 export default function MyApp() {
   return (
     <React.Fragment>
       <CssBaseline />
-      
+      <ButtonAppBar />
+      <ImageMasonry />
+      <ActiveLastBreadcrumb />
+      <PaginationOutlined />
+      <FixedBottomNavigation />
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MyApp from './App';
-import ImageMasonry from './masonry';
-// import { Masonry } from '@mui/lab';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MyApp />
-    <ImageMasonry />
   </React.StrictMode>
 );
 
